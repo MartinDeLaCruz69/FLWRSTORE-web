c2807eab7ce76ab96ccd4dfb7c94507b4b46210a
Juan Martin Esparza de la Cruz <martin.esparza@iea.gob.mx>
2026-04-22 09:07:33 -0600

    [Implement] Vue Router setup and main views (Implementación de Vue Router y vistas principales: Home, Legal, Stock)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <nav>
+    <router-link to="/">Inicio</router-link> |
+    <router-link to="/stock">Stock</router-link> |
+    <router-link to="/legal">Legal</router-link>
+  </nav>
+  <router-view></router-view>
 </template>
