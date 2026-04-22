d600870a653b2aeb55a2ac19e9e0a2b5b963bbfc
Juan Martin Esparza de la Cruz <martin.esparza@iea.gob.mx>
2026-04-22 10:55:49 -0600

    [Implement] Impement of Payment methods view (Implementación de la vista de formas de pago)

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,9 @@
 <template>
-  <div id="app">
-    <h1>🌸 FLWRSTORE - Panel de Control 🌸</h1>
-    <p>Estado de la conexión: <strong>{{ status }}</strong></p>
-  </div>
+  <Navbar /> 
+  
+  <router-view /> 
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { db } from './firebase'; // Asegúrate de que la ruta sea correcta
-import { collection, getDocs } from 'firebase/firestore';
-
-const status = ref('Cargando...');
-
-onMounted(async () => {
-  try {
-    // Intentamos leer cualquier cosa de una colección (aunque no exista)
-    await getDocs(collection(db, 'test'));
-    status.value = '¡CONEXIÓN EXITOSA! 🔥';
-  } catch (error) {
-    console.error("Error detallado:", error);
-    status.value = 'Error de conexión: ' + error.message;
-  }
-});
+import Navbar from './components/Navbar.vue'
 </script>
-
-<style>
-#app {
-  font-family: sans-serif;
-  text-align: center;
-  margin-top: 50px;
-  color: #2c3e50;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router' // Importa el router
+import router from './router' // Importa la configuración que hicimos
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router) // ¡Asegúrate de que esta línea exista!
+app.mount('#app')
